b6ab62a3b3a90903cb1325df69ce4e8c4d6a20b4
OSWALDO JUNIOR <genesisitsolutions@hotmail.com>
2018-03-24 10:49:18 -0300

    Craiação do controller, para meu cadastro de negociação.

--- client/js/models/Negociacao.js
+++ client/js/models/Negociacao.js
@@ -1,7 +1,17 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get data() {
+        return new Date(this._data.getTime());
     }
 }
